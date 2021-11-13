94f0358cd43c110f9295880a7a4481ade792be58
wchamara <chamara.weerasinghe@my.sliit.lk>
2021-11-13 12:30:01 +0530

    Update index.js
    added a function to find habitable planets

--- Planets_projects/index.js
+++ Planets_projects/index.js
@@ -1,1 +1,34 @@
-const parse = require('csv-parse')
+const parse = require("csv-parse");
+const fs = require("fs");
+
+const HabitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("keplor_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      HabitablePlanets.push(data);
+    }
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    console.log(`${HabitablePlanets.length} habitable planets found`);
+  });
+
+
