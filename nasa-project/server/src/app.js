54ff7fe6bc9899aff9162319201e780c1c7575ae
chamara <chamara.weerasinghe@my.sliit.lk>
2022-01-24 20:15:47 +0530

    added th efirst enpoint

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const planetRouter = require("./routes/planets/planets.routes");
-
+const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(planetRouter);
 module.exports = app;
