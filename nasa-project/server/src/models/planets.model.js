c0f5d19b12e382132255b983cc3d851de97174c0
chamara <chamara.weerasinghe@my.sliit.lk>
2022-01-25 04:58:41 +0530

    adding planet-project to the code

--- nasa-project/server/src/models/planets.model.js
+++ nasa-project/server/src/models/planets.model.js
@@ -1,3 +1,41 @@
-const planets = [];
+const parse = require("csv-parse");
+const fs = require("fs");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+fs.createReadStream("keplor_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    const planetNames = habitablePlanets.map((planet) => ({
+      kepler_name: planet.kepler_name,
+      kepoi_name: planet.kepoi_name,
+    }));
+    console.table(planetNames);
+    console.log(`${habitablePlanets.length} habitable planets found`);
+  });
+
+module.exports = {
+  planets: habitablePlanets,
+};
