839d57129192f4feb9a289ee24fe1b8dbcbe4b86
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-02-01 10:25:59 +0530

    adding small changes to the core structure

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const path = require("path");
+
 const planetRouter = require("./routes/planets/planets.routes");
 const cors = require("cors");
 const app = express();
@@ -9,5 +11,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetRouter);
 module.exports = app;
