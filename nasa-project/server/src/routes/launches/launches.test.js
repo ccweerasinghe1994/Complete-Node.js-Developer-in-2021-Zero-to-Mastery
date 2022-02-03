fc2fa07ad03a5745cb6b780471e8f8d6ef669cc5
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-03 20:20:39 +0530

    adding jest and test files for the launches

---
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe("Test GET /launches", () => {
+  test("it should respond with 200 success", () => {
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("it should respond with 200 success", () => {});
+
+  test("it should catch missing properties", () => {});
+
+  test("it should catch invalid dates", () => {});
+});
