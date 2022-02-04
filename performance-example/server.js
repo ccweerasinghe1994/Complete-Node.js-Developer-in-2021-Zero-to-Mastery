21c2d4b197017e193abf1b25d771cf4dcfc69cfd
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-04 08:17:19 +0530

    pm2 commands study

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,8 +1,5 @@
 const express = require("express");
 
-const cluster = require("cluster");
-const os = require("os");
-
 const app = express();
 
 function delay(duration) {
@@ -19,19 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/timer", (req, res) => {
   //   delay the response
-  delay(9000);
-  res.send(`Ding Ding Ding ${process.pid}`);
+  delay(4000);
+  res.send(`Beep Beep Beep ${process.pid}`);
 });
 
-if (cluster.isMaster) {
-  console.log("Master has been started");
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("worker has been started");
-  app.listen(3000, () => {
-    // console.log(`server is running on port 3000`);
-  });
-}
+console.log("worker has been started");
+app.listen(3000, () => {
+  // console.log(`server is running on port 3000`);
+});
