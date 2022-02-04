77e20a67836466ce396446fd7ebb864a3dbc2cb7
chamara <chamara.weerasinghe@my.sliit.lk>
2022-02-04 07:22:13 +0530

    adding os module to the codebase

--- performance-example/server.js
+++ performance-example/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const cluster = require("cluster");
+const os = require("os");
+
 const app = express();
 
 function delay(duration) {
@@ -11,15 +14,24 @@ function delay(duration) {
 }
 
 app.get("/", (req, res) => {
-  res.send("performance example");
+  res.send(`performance example ${process.pid}`);
 });
 
 app.get("/timer", (req, res) => {
   //   delay the response
   delay(9000);
-  res.send("Ding Ding Ding");
+  res.send(`Ding Ding Ding ${process.pid}`);
 });
 
-app.listen(3000, () => {
-  console.log(`server is running on port 3000`);
-});
+if (cluster.isMaster) {
+  console.log("Master has been started");
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log("worker has been started");
+  app.listen(3000, () => {
+    // console.log(`server is running on port 3000`);
+  });
+}
