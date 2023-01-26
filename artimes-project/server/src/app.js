38240931bfde098a81cd4ff0698d7e7784df65ef
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-26 08:12:39 +0530

    adding morgan for logging

--- artimes-project/server/src/app.js
+++ artimes-project/server/src/app.js
@@ -1,11 +1,21 @@
-import express, { json } from 'express';
-import cors from 'cors'
-import { planetRouter } from './routes/planets/planets.router.js';
+import express, { json, static as staticFileServer } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors';
+import morgan from 'morgan'
 
+import { planetRouter } from './routes/planets/planets.router.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express({
   origin: 'http://localhost:3000',
 });
-app.use(cors())
+app.use(cors());
+app.use(morgan('combined'))
 app.use(json());
+app.use(staticFileServer(path.join(__dirname,'..','public')));
 app.use(planetRouter);
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'..','public','index.html'))
+})
 export { app };
