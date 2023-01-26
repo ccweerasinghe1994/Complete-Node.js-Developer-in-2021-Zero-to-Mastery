869816dbb87091eff0290058ffb10adbb1615822
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-01-26 05:53:15 +0530

    adding cors

--- artimes-project/server/src/app.js
+++ artimes-project/server/src/app.js
@@ -1,7 +1,11 @@
 import express, { json } from 'express';
+import cors from 'cors'
 import { planetRouter } from './routes/planets/planets.router.js';
 
-const app = express();
+const app = express({
+  origin: 'http://localhost:3000',
+});
+app.use(cors())
 app.use(json());
 app.use(planetRouter);
 export { app };
