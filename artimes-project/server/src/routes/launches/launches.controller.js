85015841b27d12db70d503e2f2f173082d5d8489
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-02-14 10:29:37 +0530

    Update launches.controller.js

--- artimes-project/server/src/routes/launches/launches.controller.js
+++ artimes-project/server/src/routes/launches/launches.controller.js
@@ -1,40 +1,42 @@
 import {
-    getAllLaunches,
-    addNewLaunch,
-    isLaunchExists,
+  getAllLaunches,
+  addNewLaunch,
+  isLaunchExists,
+  abortLaunchById,
 } from '../../models/launches.model.js';
 
 function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches());
+  return res.status(200).json(getAllLaunches());
 }
 
 function httpAddNewLaunch(req, res) {
-    const {body} = req;
+  const { body } = req;
 
-    if (!body.mission || !body.rocket || !body.target || !body.launchDate) {
-        res.status(401).json({
-            error: 'missing required fields',
-        });
-    }
+  if (!body.mission || !body.rocket || !body.target || !body.launchDate) {
+    res.status(401).json({
+      error: 'missing required fields',
+    });
+  }
 
-    body.launchDate = new Date(body.launchDate);
-    if (isNaN(body.launchDate)) {
-        res.status(401).json({
-            error: 'invalid launch date',
-        });
-    }
-    addNewLaunch(body);
-    return res.status(201).json(body);
+  body.launchDate = new Date(body.launchDate);
+  if (isNaN(body.launchDate)) {
+    res.status(401).json({
+      error: 'invalid launch date',
+    });
+  }
+  addNewLaunch(body);
+  return res.status(201).json(body);
 }
 
 function httpDeleteLaunch(req, res) {
-    const id = req.params.id;
-    if (!isLaunchExists(id)) {
-        return res.status(404).json({
-            error: 'Launch Not Found',
-        });
-    }
-    return res.status(200).json(aborted);
+  const id = +req.params.id;
+  if (!isLaunchExists(id)) {
+    return res.status(404).json({
+      error: 'Launch Not Found',
+    });
+  }
+  const aborted = abortLaunchById(id);
+  return res.status(200).json(aborted);
 }
 
-export {httpGetAllLaunches, httpAddNewLaunch, httpDeleteLaunch};
+export { httpGetAllLaunches, httpAddNewLaunch, httpDeleteLaunch };
