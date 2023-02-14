cf058853134969c21b5a20755a6ef20c3456788a
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-02-14 07:46:53 +0530

    Update launches.router.js

--- artimes-project/server/src/routes/launches/launches.router.js
+++ artimes-project/server/src/routes/launches/launches.router.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { httpGetAllLaunches, httpAddNewLaunch } from './launches.controller.js';
+import { httpGetAllLaunches, httpAddNewLaunch, httpDeleteLaunch} from './launches.controller.js';
 
 const launchesRouter = Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
-
+launchesRouter.delete('/:id',httpDeleteLaunch)
 export { launchesRouter };
